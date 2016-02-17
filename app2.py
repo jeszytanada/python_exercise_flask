1f16271ca0302073c49b13b1b1286b7bbfc713ef
Maryjes Tanada <maryjestanada@MAC004.local>
2016-02-17 13:49:46 +0800

    Exercise1

---
+++ app2.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, session
+app = Flask(__name__)
+#request.data
+#url_for(endpoint , **kwargs)
+@app.route('/base/')
+def hello():
+	#assert False
+    return render_template('base.html')
+
+@app.route('/<name>/<int:answer>',methods=['GET', 'POST'])
+def guess(name, answer):
+	#assert False
+    if name == 'jeszy':
+        session['logged_in'] = True
+        fname = request.form['fname']
+        lname = request.form['lname']   
+        correct = (answer == 27)
+    return render_template('guess2.html', name=name, correct=correct)
+
+@app.route('/store/',methods=['GET', 'POST'])
+def store():
+
+    return render_template('store.html')
+    
+
+app.secret_key = 'yeyow'
+
+if __name__ == '__main__':
+    app.run(debug=True)
